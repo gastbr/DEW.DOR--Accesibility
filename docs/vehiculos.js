ea6727c10e18255b74f1c546befcc39583229efe
WACHIDADUCK <captainvantruc@gmail.com>
2024-11-28 22:45:33 +0000

    Imagenes vehiculos,planetas + figma en el readme

--- docs/vehiculos.js
+++ docs/vehiculos.js
@@ -22,12 +22,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 // Crear el contenido de la tarjeta
                 vehicleCard.innerHTML = `
-                    <div class="vehicle-card-body">
-                        <h3>${vehicle.name}</h3>
-                        <p><strong>Model:</strong> ${vehicle.model}</p>
-                        <p><strong>Manufacturer:</strong> ${vehicle.manufacturer}</p>
-                        <p><strong>Cost in credits:</strong> ${vehicle.cost_in_credits}</p>
-                        <p><strong>Max speed:</strong> ${vehicle.max_atmosphering_speed} km/h</p>
+
+                    <div class="card mb-3 ">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="${urlImgVehiculo(vehicle.name)}"
+                                    class="img-fluid rounded-start" alt="${vehicle.name}"
+                                    onerror="this.onerror=null; this.src='https://via.placeholder.com/300x200';">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">${vehicle.name}</h5>
+                                    <p class="card-text"><strong>Model:</strong> ${vehicle.model}</p>
+                                    <p class="card-text"><strong>Manufacturer:</strong> ${vehicle.manufacturer}n</p>
+                                    <p class="card-text"><strong>Cost in credits:</strong> ${vehicle.cost_in_credits}</p>
+                                    <p class="card-text"><strong>Max speed:</strong> ${vehicle.max_atmosphering_speed}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 `;
 
@@ -45,3 +57,30 @@ document.addEventListener('DOMContentLoaded', () => {
     // Llamar a la función para obtener los vehículos
     fetchVehicles();
 });
+
+
+function urlImgVehiculo(nombre){
+    if(nombre=="Sand Crawler"){
+        return "https://starwars-visualguide.com/assets/img/vehicles/4.jpg"
+    }else if(nombre=="T-16 skyhopper"){
+        return "https://starwars-visualguide.com/assets/img/vehicles/6.jpg"
+    }else if(nombre=="X-34 landspeeder"){
+        return "https://starwars-visualguide.com/assets/img/vehicles/7.jpg"
+    }else if(nombre=="TIE/LN starfighter"){
+        return "https://diceandcardboard.com/content/images/2022/06/7D447526-4083-44FC-BAAD-FEBA5A3872B0.jpeg"
+    }else if(nombre=="Snowspeeder"){
+        return "https://starwars-visualguide.com/assets/img/vehicles/14.jpg"
+    }else if(nombre=="TIE bomber"){
+        return "https://starwars-visualguide.com/assets/img/vehicles/16.jpg"
+    }else if(nombre=="AT-AT"){
+        return "https://static.posters.cz/image/1300/posters/star-wars-at-at-attack-i67847.jpg"
+    }else if(nombre=="AT-ST"){
+        return "https://starwars-visualguide.com/assets/img/vehicles/19.jpg"
+    }else if(nombre=="Storm IV Twin-Pod cloud car"){
+        return "https://starwars-visualguide.com/assets/img/vehicles/20.jpg"
+    }else if(nombre=="Sail barge"){
+        return "https://starwars-visualguide.com/assets/img/vehicles/24.jpg"
+    }else if(nombre==""){
+        return ""
+    }
+}
